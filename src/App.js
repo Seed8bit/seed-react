042d592eda1372130278f1af7f96781fe3b7bfd1
Runze <sendrunzedong@gmail.com>
2020-06-19 21:57:38 -0400

    Add eslint

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import {Card, Button, Nav} from 'react-bootstrap'
-import {vegetableList} from './vegeInfo'
+import {Card, Button} from 'react-bootstrap';
+import {vegetableList} from './vegeInfo';
 
 
 class App extends React.Component {
-
   render() {
-    
-    let displayItem = vegetableList.map((element) => {
+    const displayItem = vegetableList.map((element) => {
       return (
-      <div style = {{margin: '2rem'}}>
-        <Card style={{ width: '10rem'}}>
-        <Card.Img variant="top" src={element.icon}/>
-        <Card.Body>
-          <Card.Title>{element.name}</Card.Title>
-          <Card.Text>
+        <div style = {{margin: '2rem'}} key = {element.name}>
+          <Card style={{width: '10rem'}}>
+            <Card.Img variant="top" src={element.icon}/>
+            <Card.Body>
+              <Card.Title>{element.name}</Card.Title>
+              <Card.Text>
             TestTest
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-        </Card>
-      </div>);
-    })
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </div>);
+    });
 
     return (
       <div style={{display: 'flex', margin: '3rem'}}>
--- src/vegeInfo.js
+++ src/vegeInfo.js
@@ -1,35 +1,35 @@
 export const vegetableList = [
   {
-    "name": "黄瓜",
-    "page": "/pages/generalInfo/generalInfo",
-    "icon": "https://i.ibb.co/MSs4M3Q/cucumber-icon.jpg",
-    "generalInfoTable": {
-      "温度": "24度到29度",
-      "湿度": "保持土壤湿润",
-      "光照": "每天需要阳光3小时",
-      "成熟": "30-50天结果"
-    }
+    'name': '黄瓜',
+    'page': '/pages/generalInfo/generalInfo',
+    'icon': 'https://i.ibb.co/MSs4M3Q/cucumber-icon.jpg',
+    'generalInfoTable': {
+      '温度': '24度到29度',
+      '湿度': '保持土壤湿润',
+      '光照': '每天需要阳光3小时',
+      '成熟': '30-50天结果',
+    },
   },
   {
-    "name": "西红柿",
-    "page": "/pages/generalInfo/generalInfo",
-    "icon": "https://i.ibb.co/ThMbSqW/tomato-icon.jpg",
-    "generalInfoTable": {
-      "温度": "10度到20度",
-      "湿度": "保持土壤中度湿润",
-      "光照": "每天需要阳光1小时",
-      "成熟": "50-60天结果"
-    }
+    'name': '西红柿',
+    'page': '/pages/generalInfo/generalInfo',
+    'icon': 'https://i.ibb.co/ThMbSqW/tomato-icon.jpg',
+    'generalInfoTable': {
+      '温度': '10度到20度',
+      '湿度': '保持土壤中度湿润',
+      '光照': '每天需要阳光1小时',
+      '成熟': '50-60天结果',
+    },
   },
   {
-    "name": "茄子",
-    "page": "/pages/generalInfo/generalInfo",
-    "icon": "https://i.ibb.co/rFLW9VY/eggplant-icon.jpg",
-    "generalInfoTable": {
-      "温度": "13度到20度",
-      "湿度": "保持土壤湿润",
-      "光照": "每天需要阳光7小时",
-      "成熟": "50-80天结果"
-    }
-  }
-]
+    'name': '茄子',
+    'page': '/pages/generalInfo/generalInfo',
+    'icon': 'https://i.ibb.co/rFLW9VY/eggplant-icon.jpg',
+    'generalInfoTable': {
+      '温度': '13度到20度',
+      '湿度': '保持土壤湿润',
+      '光照': '每天需要阳光7小时',
+      '成熟': '50-80天结果',
+    },
+  },
+];
