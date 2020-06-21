420cd26fed4c57a94d7f55852000610521971b10
Runze <sendrunzedong@gmail.com>
2020-06-21 18:28:29 -0400

    Add Toast and Spinner as notification component

---
+++ src/components/notification/Spinner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Spinner as Bspinner} from 'react-bootstrap';
+
+export default function Spinner() {
+  return (
+    <Bspinner animation='border' variant='primary'/>
+  );
+}
