599ff682cf00e3a98fe4490310d13e669a465c1b
JizhouZhang <rectcream@gmail.com>
2020-06-29 21:07:35 -0700

    added carousel support in my garden page

--- src/MyGarden.js
+++ src/MyGarden.js
@@ -1,14 +1,29 @@
 import React, {useState} from 'react';
 import ReactMarkdown from 'react-markdown';
 import {Container, Row, Col, ProgressBar
-  , Pagination, Nav} from 'react-bootstrap';
+  , Pagination, Nav, Carousel} from 'react-bootstrap';
 import {testData} from './TestData';
+import './myGardenStyle.css';
 
 function markdownExtractor(str) {
   const pages = str.split(/(?=#\s+)/);
   const content = [];
   pages.forEach((element) => {
-    content.push({'title': element.split('\n')[0], 'paragraphs': element});
+    // the following regex expression explain:
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/match
+    // https://stackoverflow.com/questions/12059284/get-text-between-two-rounded-brackets
+    const slides = [];
+    const regRes = element.match(/!\[.*\]\(.*\)/g);
+    if (regRes) {
+      regRes.forEach((slide) => {
+        const alt = slide.match(/\[([^\]]+)\]/)[1];
+        const url = slide.match(/\(([^)]+)\)/)[1];
+        slides.push({'url': url, 'alt': alt});
+      });
+    }
+    content.push({'title': element.split('\n')[0],
+      'paragraphs': element.replace(/!\[.*\]\(.*\)/g, ''),
+      'slides': slides});
   });
   return content;
 }
@@ -42,6 +57,31 @@ export default function MyGarden() {
   const handleActivePageSel = (selection) => setActivePage(selection);
 
   const paginationItems = [];
+  const slides = [];
+
+  for (let number = 0; number < content.length; number++) {
+    if (content[number]['slides'].length > 0) {
+      const carouselItems = [];
+      content[number]['slides'].forEach((element) => {
+        carouselItems.push(
+            <Carousel.Item>
+              <img
+                src={element.url}
+                alt={element.alt}
+              />
+              <Carousel.Caption>
+                <p>{element.alt}</p>
+              </Carousel.Caption>
+            </Carousel.Item>);
+      });
+      slides.push(
+          <Carousel>
+            {carouselItems}
+          </Carousel>);
+    } else {
+      slides.push(<></>);
+    }
+  }
 
   for (let number = 1; number <= content.length; number++) {
     paginationItems.push(
@@ -63,7 +103,7 @@ export default function MyGarden() {
   // "activePage - 1" to convert to index
   return (
     <>
-      <Container fluid>
+      <Container fluid style={{maxWidth: 1250}}>
         <Row>
           <Col sm={2} xs={2}>
             <MyGardenSide/>
@@ -73,9 +113,12 @@ export default function MyGarden() {
               <ProgressBar now={activePage*100/content.length}
                 label={content[activePage - 1]['title']}/>
               <Row>
-                <Col>
+                <Col sm={8} xs={8} md={8} lg={8}>
                   <ReactMarkdown
                     source={content[activePage - 1]['paragraphs']}/>
+                </Col>
+                <Col sm={4} xs={4} md={4} lg={4}>
+                  {slides[activePage - 1]}
                 </Col>
               </Row>
               <Row>
