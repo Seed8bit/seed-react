1994b7a674a676002be85712139aef9174cde8ce
Runze <sendrunzedong@gmail.com>
2020-07-05 18:05:32 -0400

    Disable lint

--- src/context/useBreedInfo.js
+++ src/context/useBreedInfo.js
@@ -1,29 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 import {getBreedInfo} from '../api/breedInfoApi';
 
 const useBreedInfo = (initialQuery) => {
-    const [loading, setLoading] = useState(false);
-    const [hasError, setHasError] = useState(false);
-    const [query, setQuery] = useState(initialQuery);
-    const [breedInfo, setBreedInfo] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const [query, setQuery] = useState(initialQuery);
+  const [breedInfo, setBreedInfo] = useState({});
 
-    useEffect(() => {
-        setLoading(true);
-        setHasError(false);
-        const fetchData = async () => {
-            try {
-                const breedInfoResponse = await getBreedInfo(query);
-                setBreedInfo(breedInfoResponse.data)
-            } catch (error) {
-                console.log(error);
-                setHasError(true);
-            }
-            setLoading(false);
-        }
-        fetchData();
-    }, [query]);
+  useEffect(() => {
+    setLoading(true);
+    setHasError(false);
+    const fetchData = async () => {
+      try {
+        const breedInfoResponse = await getBreedInfo(query);
+        setBreedInfo(breedInfoResponse.data);
+      } catch (error) {
+        console.log(error);
+        setHasError(true);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [query]);
 
-    return [{breedInfo, loading, hasError}, setQuery]
-}
+  return [{breedInfo, loading, hasError}, setQuery];
+};
 
-export { useBreedInfo };
+export {useBreedInfo};
