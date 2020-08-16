b33c35397f6e5908111feb34959bb0b0b677f2ee
Runze <sendrunzedong@gmail.com>
2020-08-16 17:06:44 -0400

    Tweak frontend routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import {render} from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import {BrowserRouter} from 'react-router-dom';
 
-render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
